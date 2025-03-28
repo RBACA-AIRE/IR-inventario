07194585a8ae7adae1601ef1481e755429c4e7fb
RBACA-AIRE <ray.baca@manantial.pe>
2025-03-28 12:02:19 -0500

    -avance basico

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+//agregar clase flotante al elemento de lista seleccionado
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+    list.forEach(item => {
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+}
+
+list.forEach(item => item.addEventListener("mouseover", activeLink));
+
+//Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function(){
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+};
